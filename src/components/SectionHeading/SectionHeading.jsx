abb79578beb0d77e5441edde303875a67f3200c1
Milan Bhandari <153265129+basmilbhan@users.noreply.github.com>
2026-03-06 09:14:34 +0545

    Add h4 title element to SectionHeading component

--- src/components/SectionHeading/SectionHeading.jsx
+++ src/components/SectionHeading/SectionHeading.jsx
@@ -5,8 +5,8 @@ const SectionHeading = ({title}) => {
   return (
     <div className="container">
       <div className={`st-section-heading st-style1`} data-aos="fade-up" data-aos-duration="800">
-        <h4 className="st-section-heading-title">{title}</h4>
         <h2 className="st-section-heading-subtitle">{title}</h2>
+        <h4 className="st-section-heading-title">{title}</h4>
       </div>
       <div className="st-height-b25 st-height-lg-b25"></div>
     </div>
